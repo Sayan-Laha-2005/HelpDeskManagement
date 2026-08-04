05df94af0a18aa2367d83313c0c049c1dab742e5
Sayan-Laha-2005 <sayanlahaclash@gmail.com>
2026-08-04 13:39:55 +0530

    Add TicketConstants for valid priorities and statuses

---
+++ HelpDesk.Api/Models/TicketConstants.cs
@@ -0,0 +1,8 @@
+namespace HelpDesk.Api.Models
+{
+    public static class TicketConstants
+    {
+        public static readonly string[] ValidPriorities = { "Low", "Medium", "High" };
+        public static readonly string[] ValidStatuses = { "Open", "In Progress", "Closed" };
+    }
+}
